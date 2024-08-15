d416e928d147234a5851dc8719e1ade7364f5309
Adwyness <117654682+Adwyness@users.noreply.github.com>
2024-08-15 17:02:02 +0100

    Update step1.0_readfastq.R
    inserted version prompt

--- R/step1.0_readfastq.R
+++ R/step1.0_readfastq.R
@@ -46,6 +46,8 @@ Choose from: \nKingdom, \nPhylum, \nClass, \nOrder, \nFamily, Genus, \nSpecies \
   sink(dataCleanfile, append = TRUE)
   cat(as.character(Sys.time()))
   cat("\n\n")
+  cat("eDNA2IQI Package Version:", as.character(packageVersion("eDNA2IQI")))
+  cat("\n\n")
   cat("Parameter options used: \n")
   utils::write.table(parameteroptions[c(1:3)], file = dataCleanfile,
                     row.names = FALSE, quote = FALSE, sep = "\t", append = TRUE,col.names = TRUE)
