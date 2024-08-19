f957776a4e88168088ece8af2b0f2c9a21662589
Adwyness <117654682+Adwyness@users.noreply.github.com>
2024-08-19 16:02:48 +0100

    Update step2.0_annotate_asvs.r

--- R/step2.0_annotate_asvs.r
+++ R/step2.0_annotate_asvs.r
@@ -112,7 +112,7 @@ taxaplot <- ggplot2::ggplot(reordered_data, aes(x = SampleID, y = Total_Abundanc
        y = "Read Count",
        fill = "Taxa")
 
-ggplot2::ggsave(file = file.path(folder,"/outputData/raw_read_taxaplot.png"), plot = taxaplot, height = 8, units = "in")
+ggplot2::ggsave(filename = file.path(folder,"/outputData/raw_read_taxaplot.png"), plot = taxaplot, height = 8, units = "in")
 
 messageColour("Raw Taxa Plot saved in Output folder \n\n", "message")
 
