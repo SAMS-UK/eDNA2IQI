403f7e408e7862b9a8a84c3aa8feb48e466525ee
Adwyness <117654682+Adwyness@users.noreply.github.com>
2024-08-22 14:37:27 +0100

    Add files via upload
    catches files already present

--- R/edna2iqi.r
+++ R/edna2iqi.r
@@ -21,7 +21,7 @@ edna2iqi=function(folder, parameteroptions) {
 if (substr(folder, nchar(folder), nchar(folder)) != "/") {
   folder <- paste0(folder, "/")
 }
- 
+
 #instate parameteroptions
  if (missing(parameteroptions)) {
     utils::data("parameteroptions", envir = environment())
@@ -29,10 +29,35 @@ if (substr(folder, nchar(folder), nchar(folder)) != "/") {
 
   message("Wrapper function: edna2iqi")
 
+  if (file.exists(file.path(paste(folder, "outputData/collated_asv_batches.rda", sep="")))) {
+    #this will error if additional samples to the same batch are copied into the test folder, without resetting
+    #could error check this, based on sample names comparison.
+    #if samples are added, then the whole batch would need to be redone.
+
+    messageColour("Data already denoised, delete 'intermediate' folder and files to repeat, \n\n", "warnMessage")
+
+  } else
+
+  {
   myASVs <- step1.0_readFastq(folder, parameteroptions)
+  }
+
+  if (file.exists(file.path(paste(folder, "outputData/taxaAllocatedReads_Family.rda", sep="")))) {
+    #this will error if additional samples to the same batch are copied into the test folder, without resetting
+    #could error check this, based on sample names comparison.
+    #if samples are added, then the whole batch would need to be redone.
+
+    messageColour("Data already taxa allocated, delete 'taxaAllocatedReads_*' files to repeat, \n\n", "warnMessage")
+
+  } else
+
+  {
 
   myTaxa <- step2.0_annotate_ASVs(folder,parameteroptions, myASVs)
+  }
 
+  load(file.path(paste(folder, "outputData/taxaAllocatedReads_Family.rda", sep="")))
+  myTaxa <- S16_reads
   myPreds <- step3.0_predict_iqi(folder, myTaxa)
 
   message("edna2iqi finished")
