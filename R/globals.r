dc6c7c6ceae1189c4139ec509b16f072adce5799
Adwyness <117654682+Adwyness@users.noreply.github.com>
2024-08-26 15:34:23 +0100

    Update globals.r

--- R/globals.r
+++ R/globals.r
@@ -1,1 +1,1 @@
-utils::globalVariables(c("Kingdom", "RF_final_reduced", "Unique"))
+utils::globalVariables(c("Kingdom", "RF_final_reduced", "Unique", "Combined_ASV_batches", "S16_reads", "SampleID", "Taxon", "reads", "desc", "Total_Abundance"))
