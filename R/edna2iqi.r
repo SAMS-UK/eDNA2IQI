77c9098d610aef34121468f8707d58e9ab36170d
Adwyness <117654682+Adwyness@users.noreply.github.com>
2025-01-30 16:17:25 +0000

    Update edna2iqi.r
    ASVs will be loaded if  step already completed

--- R/edna2iqi.r
+++ R/edna2iqi.r
@@ -1,5 +1,5 @@
-#' Wrapper function to perform all steps of Reading, denoising,
-#' allocating taxa and predicting IQI
+#' Wrapper function to perform all steps of reading and denoising raw reads,
+#' allocating taxa and predicting IQI from multiple random forests
 #'
 #'
 #' @param folder A string. Location of raw fastq data files.
@@ -19,7 +19,7 @@
 
 edna2iqi=function(folder, parameteroptions, auto_download = FALSE) {
 
-####Check Folder name and add / at the end if its not there
+####Check Folder name and add / at the end if its not there (needed for some functions)
 
 if (substr(folder, nchar(folder), nchar(folder)) != "/") {
   folder <- paste0(folder, "/")
@@ -31,39 +31,35 @@ if (substr(folder, nchar(folder), nchar(folder)) != "/") {
   }
 
   message("Wrapper function: edna2iqi")
-
+#check if step 1 completed
   if (file.exists(file.path(paste(folder, "outputData/collated_asv_batches.rda", sep="")))) {
-    #this will error if additional samples to the same batch are copied into the test folder, without resetting
-    #could error check this, based on sample names comparison.
-    #if samples are added, then the whole batch would need to be redone.
-
+  
     messageColour("Data already denoised, delete 'intermediate' folder and files to repeat, \n\n", "warnMessage")
-
+    #load step1 output if step1 already completed 
+    load(file.path(paste(folder, "outputData/collated_asv_batches.rda", sep="")))
+    myASVs <- Combined_ASV_batches
   } else
 
   {
   myASVs <- step1.0_readFastq(folder, parameteroptions, auto_download = auto_download)
   }
 
-
+#check if step 2 completed
   if (file.exists(file.path(paste(folder, "outputData/taxaAllocatedReads_Family.rda", sep="")))) {
-    #this will error if additional samples to the same batch are copied into the test folder, without resetting
-    #could error check this, based on sample names comparison.
-    #if samples are added, then the whole batch would need to be redone.
-
+    
     messageColour("Data already taxa allocated, delete 'taxaAllocatedReads_*' files to repeat, \n\n", "warnMessage")
-    load(file.path(paste(folder, "outputData/collated_asv_batches.rda", sep="")))
-    myASVs <- Combined_ASV_batches
+   #load step2 output if step2 already completed 
+    load(file.path(paste(folder, "outputData/taxaAllocatedReads_Family.rda", sep="")))
+    myTaxa <- S16_reads
 
   } else
 
   {
   load(file.path(paste(folder, "outputData/collated_asv_batches.rda", sep="")))
   myASVs <- Combined_ASV_batches
-
   myTaxa <- step2.0_annotate_ASVs(folder,parameteroptions, myASVs)
   }
-
+#step 3
   load(file.path(paste(folder, "outputData/taxaAllocatedReads_Family.rda", sep="")))
   myTaxa <- S16_reads
   myPreds <- step3.0_predict_iqi_multiple(folder, myTaxa)
