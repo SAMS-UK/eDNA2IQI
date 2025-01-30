cc16ac77e68519dcf510c5bc5d386e0213832d5c
Adwyness <117654682+Adwyness@users.noreply.github.com>
2025-01-30 17:01:23 +0000

    Update step1.2_remove_unspecified.r

--- R/step1.2_remove_unspecified.r
+++ R/step1.2_remove_unspecified.r
@@ -6,47 +6,42 @@
 #' @return A list.
 
 step1.2_remove_unspecified <- function(folder, raw_files) {
-#  browser()
-
+  
   messageColour("Function starting: 'remove_unspecified'
            Removing unspecified bases from sequence reads \n\n", "message")
 
-  # Set file to store details of cleaning and filtering
+  # Set file to store denoising details
   dataCleanfile <- paste(folder, "outputData/dataCleaningDetails.txt",sep="")
   sink(dataCleanfile, append = TRUE)
   cat(as.character(Sys.time()))
   cat("\n\nFunction 1.2: remove_unspecified \n\n")
   sink()
 
-
+  
   # Set file name of raw data in input folder
   AA=unlist(raw_files)
 
   fwd_input <- paste(folder, AA[grep("_R1_001.fastq.gz",AA,ignore.case = TRUE)],sep="")
   rev_input <- paste(folder, AA[grep("_R2_001.fastq.gz",AA,ignore.case = TRUE)],sep="")
 
-
   # Set file name of raw data in intermediate folder
   path_no_unspec <- paste(folder, "outputData/intermediate/no_Ns",sep="")
-
 
   if (!dir.exists(path_no_unspec)) {dir.create(path_no_unspec)}
   fwd_no_unspec <- paste(path_no_unspec, gsub("^.*/", "", fwd_input), sep = "/")
   rev_no_unspec <- paste(path_no_unspec, gsub("^.*/", "", rev_input), sep = "/")
 
 
-  # Skip step and if files already present
+  # Skip step if files already present
   if ((sum(!file.exists(fwd_no_unspec)) +
        sum(!file.exists(rev_no_unspec))) != 0) {
 
   # Remove Ns from the sequences and save as .gz files in intermediate folder
-
   dada2::filterAndTrim(fwd_input, fwd_no_unspec, rev_input, rev_no_unspec,
                        maxN = 0, multithread = FALSE, verbose = FALSE)
 
 
   # DATA FLAG: flag if <15000 reads per sample before denoising
-
   names <- c()
 
   for (x in seq_along(fwd_input)) {
@@ -56,8 +51,9 @@ step1.2_remove_unspecified <- function(folder, raw_files) {
     }
   }
 
+    #message to R console
   if (length(names) > 0) {
-    messageColour("Low raw read count for following samples.\n", "warning")
+    messageColour("Low raw read count (<15000 reads) for the following samples.\n", "warning")
 messageColour("Confidence in prediction may be low. Consider investigation of
 raw read count and denoising statistics. \n", "warning")
 for (y in names){
@@ -66,9 +62,10 @@ for (y in names){
 }
 cat("\n")
 
+    #write to details file
   sink(dataCleanfile, append = TRUE)
   cat(as.character(Sys.time()))
-  cat("\nLow raw read count for following samples.\n")
+  cat("\nLow raw read count (<15000 reads) for following samples.\n")
   cat("Confidence in prediction may be low. Consider investigation of raw read")
   cat(" count and denoising statistics.\n")
   for (y in names){
@@ -79,7 +76,7 @@ cat("\n")
   }
 
   # Add details of Ns cleaning to file
-  fw_in_L <- c()#create empty vector
+  fw_in_L <- c()
   fw_out_L <- c()
   sink(dataCleanfile, append = TRUE)
   cat(as.character(Sys.time()))
@@ -96,7 +93,7 @@ cat("\n")
   cat("\n\n")
   sink()
 
-  ####DATA FLAG: flag if data loss >70% ####
+  #Data flag: flag if data loss >70%
   if (length(names[(fw_out_L / fw_in_L) < 0.3]) > 0) {
 messageColour("High loss of reads when removing unspecified or ambigous bases in
 following samples, data of low quality. Treat IQI predictions with caution.",
@@ -119,10 +116,10 @@ following samples, data of low quality. Treat IQI predictions with caution.",
     sink()
   }
 
-  ####Check all files created ####
+  #Check all files created
   fwd_actual <- stringr::str_subset(dir(path_no_unspec), "_R1_001.fastq.gz")
   rev_actual <- stringr::str_subset(dir(path_no_unspec), "_R2_001.fastq.gz")
-  #use grep and then ignore.case.##
+  #use grep and then ignore.case.
   fwd_expected <- stringr::str_subset(raw_files, "_R1_001.fastq.gz")
   rev_expected <- stringr::str_subset(raw_files, "_R2_001.fastq.gz")
 
