bbd54f812b3f1eb18d5d99a9bf38bcb485e3f60b
Adwyness <117654682+Adwyness@users.noreply.github.com>
2025-02-03 14:21:20 +0000

    Update step2.1_taxa_allocation.r
    alphabeticise taxa names, parrallel processing possibly changes the order, and therefore rarefaction?

--- R/step2.1_taxa_allocation.r
+++ R/step2.1_taxa_allocation.r
@@ -70,7 +70,9 @@ step2.1_taxa_allocation <- function(folder, collated_asv_batches, taxalevel) {
     dplyr::select(-dplyr::contains("mitochondria"))
 # Only keep Bacteria columns
   S16_reads <- taxa_combined[, grepl("Bacteria", names(taxa_combined))]
+  S16_reads <- S16_reads[, order(names(S16_reads))]
 
+                   
   return(S16_reads)
   message("Function Step2.1_taxa_allocation finished")
 
