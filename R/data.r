8d318d836693253e192bb0fae99a68f291127127
Adwyness <117654682+Adwyness@users.noreply.github.com>
2025-08-25 13:09:37 +0100

    Update data.r

--- R/data.r
+++ R/data.r
@@ -1,19 +1,3 @@
-#' Example dataset: edna2iqi_paper
-#'
-#' A dataset bundled with the package for testing and demonstration.
-#'
-#' @format A list with two elements:
-#' \describe{
-#'   \item{data.frame}{A data frame with 762 observations and 867 variables. Each row corresponds to a sample, and each column corresponds to a taxonomic feature (e.g. ASVs, taxa).}
-#'   \item{<other_element>}{A data frame of 762 observations and 13 variables. Each row corresponds to a sample, and each column corresponds to a metadata variable, including the actual IQI, farm name, etc.}
-#' }
-#'
-#' @details
-#' dataset used in internal testing and examples for the
-#' `edna2iqiRFtrainer` package.
-#' @source BactMetBar phase 1
-"edna2iqi_paper"
-
 #' Parameter options dataset
 #'
 #' A dataset listing all parameter options for denoising functions.
