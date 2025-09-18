bca29ba36557ec1295ba4858fd53190bd07293f3
Alan MacDonald <alan.macdonald@sams.ac.uk>
2025-09-18 10:52:07 +0100

    valetudo url

--- R/downloadExternal.R
+++ R/downloadExternal.R
@@ -15,7 +15,7 @@
 # Handle thredds 1st time user reg, and store token for subsequent downloads
 # Download with token; prefers libcurl headers; falls back to curl package
 
-.flask_base      <- "http://127.0.0.1:8447" # this will be https://thredds.sams.ac.uk eventually
+.flask_base      <- "http://valetudo:8447" # this will be https://thredds.sams.ac.uk eventually
 .register_url  <- paste0(.flask_base, "/api/register")  
 .api  <- paste0(.flask_base, "/api/download") # GET with Bearer token is fine
 
