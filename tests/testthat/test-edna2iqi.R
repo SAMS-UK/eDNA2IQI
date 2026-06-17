157a293e5d7a342aac1380510fca3b082b6be67f
Foster <Tim.Foster@SEPA.org.uk>
2026-06-17 16:20:19 +0100

    tests: update test to match new output
    Add files to test outlier nmds test to filter out unusual samples and return 'NA' IQI predictions. Also, expected predicted IQI value changed since update to latest random forest model.

--- tests/testthat/test-edna2iqi.R
+++ tests/testthat/test-edna2iqi.R
@@ -1,9 +1,13 @@
 test_that("edna2iqi function works", {
-  
-  path <- list.dirs(system.file("extdata/example_data/input", package = "eDNA2IQI"), full.names = TRUE)[1]
+
+  path <- list.dirs(
+    system.file("extdata/example_data/input", package = "eDNA2IQI"),
+    full.names = TRUE)[1]
   test_data <- eDNA2IQI::edna2iqi(folder = path, auto_download = TRUE)
   preds <- test_data$myPreds
-  
-  testthat::expect_equal(round(preds$MeanIQI,4), round(c(0.3069),4))
-  
+  #
+  testthat::expect_equal(round(preds$MeanIQI,4)[1], round(c(0.3131),4))
+  # Test nmds test is removing outliers
+  testthat::expect_equal(preds$MeanIQI[2], as.numeric(NA))
 })
+
